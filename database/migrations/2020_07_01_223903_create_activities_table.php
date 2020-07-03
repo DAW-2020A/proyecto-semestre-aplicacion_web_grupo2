aba0770a3e18b81b767436825e6bdb72d67e0d57
Jmiguel14 <miguel.jurado@epn.edu.ec>
2020-07-03 00:25:35 -0500

    #3 Modelo Activity y Migración

--- database/migrations/2020_07_01_223903_create_activities_table.php
+++ database/migrations/2020_07_01_223903_create_activities_table.php
@@ -17,7 +17,7 @@ class CreateActivitiesTable extends Migration
             $table->bigIncrements('id');
             $table->string('title');
             $table->text('description');
-            $table->integer('score');
+            $table->float('score');
             $table->timestamps();
         });
     }
