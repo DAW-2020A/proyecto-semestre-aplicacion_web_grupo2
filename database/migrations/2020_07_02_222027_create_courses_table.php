6189b616d532e2f3a797ce85046fe07b5792e7ff
EstebanRios99 <esteban.rios@epn.edu.ec>
2020-07-02 23:24:47 -0500

    #4 Modelo y migración Crossword

--- database/migrations/2020_07_02_222027_create_courses_table.php
+++ database/migrations/2020_07_02_222027_create_courses_table.php
@@ -15,7 +15,6 @@ class CreateCoursesTable extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('word');
            $table->string('name');
             $table->integer('code');
             $table->timestamps();
