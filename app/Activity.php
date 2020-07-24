0d915720183be42e185f1a7192e750b441eb01a0
Jmiguel14 <miguel.jurado@epn.edu.ec>
2020-07-23 23:59:21 -0500

    #28 Relatioships-1-eloquent

--- app/Activity.php
+++ app/Activity.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class Activity extends Model
 {
     protected $fillable = ['title', 'description', 'score'];
+
+    public function completes()
+    {
+        return $this->hasMany('App\Complete');
+    }
+
+    public function multiple_choices()
+    {
+        return $this->hasMany('App\MultipleChoice');
+    }
+
+    public function crosswords()
+    {
+        return $this->hasMany('App\Crossword');
+    }
 }
+
+
--- app/Word.php
+++ app/Word.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Word extends Model
 {
     protected $fillable = ['word'];
+
+    public function word_search()
+    {
+        return $this->belongsTo('App\WordSearch');
+    }
 }
